61df71ba81ce8edcef4c674f5521ca60087d2e61
lunar0926 <lunar0926@gmail.com>
2021-10-11 02:01:15 +0900

    variable

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const a = 5;
+const b = 2;
+
+const myName = "jaehyeong";
+
+// in javascript variable convention: myName
+// in python variable convention: my_name
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
+
+
+
+
